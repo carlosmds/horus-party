047633609bf86e24ab508d4a0d528497f603b912
Carlos Medeiros <carlosmedeirosdesouza@gmail.com>
2020-06-24 15:43:57 -0300

    commit para salvar

--- server.js
+++ server.js
@@ -13,24 +13,28 @@ const log = logger.child({ level: env.LOG_LEVEL || 'info', prettyPrint: true });
 //     ca: fs.readFileSync(env.SSL_CA),
 // }, app);
 
-const server = require('http')// for localhost
+const server = require('https').createServer(app);// for localhost
 
 const io = require('socket.io')(server);
 
-const redisClient = redis.createClient({
-    host: env.REDIS_HOST,
-    port: env.REDIS_PORT,
-    no_ready_check: true,
-    auth_pass: env.REDIS_PASSWORD
-});
+// const redisClient = redis.createClient({
+//     host: env.REDIS_HOST,
+//     port: env.REDIS_PORT,
+//     no_ready_check: true,
+//     auth_pass: env.REDIS_PASSWORD
+// });
 
-redisClient.on('connect', () => {   
-    log.info("Redis connected");
-}); 
+// redisClient.on('connect', () => {   
+//     log.info("Redis connected");
+// }); 
 
-redisClient.on('error', (err) => {
-    log.error("Error " + err)
-});
+// redisClient.on('error', (err) => {
+//     log.error("Error " + err)
+// });
+
+const users = {};
+
+const socketToRoom = {};
 
 io.on('connection', socket => {
 
@@ -43,7 +47,43 @@ io.on('connection', socket => {
 
     log.debug(user, 'User connected');
     
-    socket.emit('connected', user);
+    // socket.emit('connected', user);
+
+    socket.on("join room", roomID => {
+      if (users[roomID]) {
+        const length = users[roomID].length;
+        if (length === 4) {
+            socket.emit("room full");
+            return;
+        }
+        users[roomID].push(socket.id);
+      } else {
+          users[roomID] = [socket.id];
+      }
+      
+      socketToRoom[socket.id] = roomID;
+      const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+      socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+      io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+      io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+      const roomID = socketToRoom[socket.id];
+      let room = users[roomID];
+
+      if (room) {
+          room = room.filter(id => id !== socket.id);
+          users[roomID] = room;
+      }
+    });
 });
 
 server.listen(env.PORT, () => {

4fe10096e4a7f964f01285413418edf27087753c
Carlos <carlosmedeirosdesouza@gmail.com>
2022-02-15 01:42:17 -0300

    update api

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const server = require('http').createServer(app);// for localhost
 
 const log = logger.child({ prettyPrint: true });
 
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    no_ready_check: true,
+    auth_pass: process.env.REDIS_PASSWORD
+});
+
+redisClient.on('connect', () => {   
+    log.info("Redis connected successfully!");
+}); 
+
+redisClient.on('error', (err) => {
+    log.error("Redis connection error: " + err)
+});
+
 const users = {};
 const socketToRoom = {};
 
@@ -13,67 +30,125 @@ const io = require('socket.io')(server);
 
 io.on('connection', socket => {
 
-    let user = { 
+    let user = {
         id: socket.id,
         ...socket.handshake.query
     };
 
-    log.debug(JSON.stringify(user), 'User connected');
+    emitAllRooms(socket);
+
+    log.debug(['User connected', JSON.stringify(user)]);
 
     socket.on("join room", roomID => {
-      if (users[roomID]) {
-        const length = users[roomID].length;
-        if (length === 4) {
-            socket.emit("room full");
-            return;
-        }
-        users[roomID].push(socket.id);
-      } else {
-          users[roomID] = [socket.id];
-      }
-      
-      socketToRoom[socket.id] = roomID;
-      const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
 
-      socket.emit("all users", usersInThisRoom);
-    });
+      user.room = roomID;
+      userAcceptance(socket, user);
 
-    socket.on("sending signal", payload => {
-      io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-      io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
-
-    socket.on('disconnect', () => {
-      const roomID = socketToRoom[socket.id];
-      let room = users[roomID];
-
-      if (room) {
-          room = room.filter(id => id !== socket.id);
-          users[roomID] = room;
-      }
     });
 });
+
+async function userAcceptance(socket, user)
+{
+    const previouslyConnectedUsers = await getPreviouslyConnectedUsers(user);
+
+    log.info(['previouslyConnectedUsers', previouslyConnectedUsers]);
+
+    if ((previouslyConnectedUsers.length ?? 0) == 4) {
+      socket.emit("room full");
+      return;
+    }
+
+    redisClient.set(`room:${user.room}:user:${user.id}`, JSON.stringify(user));
+    
+    let usersInThisRoom = previouslyConnectedUsers.filter(redisUser => redisUser.id !== user.id);
+
+    log.info(['userAcceptance:usersInThisRoom', usersInThisRoom]);
+  
+    socket.emit("all users", usersInThisRoom);
+
+    handleSocketRoom(socket, user)
+}
+
+function handleSocketRoom(socket, user){
+
+  socket.on("sending signal", payload => {
+    io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+  });
+
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: user.id });
+  });
+
+  socket.on('disconnect', () => {
+    redisClient.del(`room:${user.room}:user:${user.id}`);
+  });
+}
+
+function getPreviouslyConnectedUsers(user)
+{
+  return new Promise((resolve) => {
+    try {
+      redisClient.keys(`room:${user.room}:user:*`, function(err, keys) {  
+        
+        if (err) throw err;
+
+        log.info(['getPreviouslyConnectedUsers:keys', keys]);
+        
+        if (keys.length == 0) {
+          resolve([]);
+          return;
+        }
+
+        redisClient.mget(keys, function(err, users) {
+
+            if (err) throw err;
+
+            if (users.length == 0) {
+              resolve([]);
+              return;
+            }
+
+            connectedUsers = users.map(user => JSON.parse(user));
+
+            log.info(['getPreviouslyConnectedUsers:connectedUsers', connectedUsers]);
+            resolve(connectedUsers);
+        });
+      });
+
+    } catch(e) {
+        log.error(['ERROR.getPreviouslyConnectedUsers', e]);
+    }
+  });
+}
+
+async function emitAllRooms(socket)
+{
+  const rooms = await getRooms();
+  socket.broadcast.emit('all rooms', rooms)
+}
+
+function getRooms()
+{
+  return new Promise((resolve) => {
+
+    try {
+
+      redisClient.keys(`room:*`, function(err, keys) {  
+        
+        if (err) throw err;
+
+        log.info(['getRooms:keys', keys]);
+
+        resolve(keys);
+
+      });
+
+    } catch(e) {
+        log.error(['ERROR.getPreviouslyConnectedUsers', e]);
+    }
+  });
+}
 
 server.listen(process.env.PORT, () => {
   log.info('Running on port '+process.env.PORT);
 });
-
-// const redis = require('redis');
-
-// const redisClient = redis.createClient({
-//     host: process.env.REDIS_HOST,
-//     port: process.env.REDIS_PORT,
-//     no_ready_check: true,
-//     auth_pass: process.env.REDIS_PASSWORD
-// });
-
-// redisClient.on('connect', () => {   
-//     log.info("Redis connected");
-// }); 
-
-// redisClient.on('error', (err) => {
-//     log.error("Error " + err)
-// });
